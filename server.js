e153f651cb9ea7c25fc705d69fe4b2a78ca6fc60
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-10 05:05:00 -0700

    Imported productRouter and removed static product from server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const data = require("./data.js");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const userRouter = require("./routers/userRouter.js");
+const productRouter = require("./routers/productRouter.js");
 
 const app = express();
 const connectDB = async () => {
@@ -22,19 +23,7 @@ const connectDB = async () => {
 
 connectDB();
 
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
-
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
-
+app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 
 app.get("/", (req, res) => {
