5dd42892a9dfdc5e5c014e411cd8a859cf4adc0d
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-13 15:11:58 -0700

    Added a middleware to authenticate user

--- utils.js
+++ utils.js
@@ -14,3 +14,26 @@ export const generateToken = (user) => {
     }
   );
 };
+
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+      //jwt returns jwt data inside decode and put in req.user
+      (err, decode) => {
+        if (err) {
+          res.status(401).send({ message: 'Invalid Token' });
+        } else {
+          req.user = decode;
+          next();
+        }
+      }
+    );
+  } else {
+    res.status(401).send({ message: 'No Token' });
+  }
+};
