230b95d092308febc1bcc44d19c5f16bb23feb63
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-15 04:19:12 -0700

    Created an API  to send paypal client ID to frontend

--- server.js
+++ server.js
@@ -31,7 +31,9 @@ connectDB();
 app.use("/api/products", productRouter);
 app.use('/api/orders', orderRouter);
 app.use("/api/users", userRouter);
-
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
