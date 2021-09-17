f2ca471ba2a2547a55a37edd4d692b09120fb3c2
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-17 06:06:29 -0700

    Defined a new router to upload file. Also, added upload directory for better communication to frontend

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import path from 'path';
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 import orderRouter from './routers/orderRouter.js';
+import uploadRouter from "./routers/uploadRouter.js";
 
 dotenv.config();
 
@@ -28,12 +30,15 @@ const connectDB = async () => {
 
 connectDB();
 
+app.use('/api/uploads', uploadRouter);
 app.use("/api/products", productRouter);
 app.use('/api/orders', orderRouter);
 app.use("/api/users", userRouter);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
