52271a34ba9d3e61a169623b6f6f65965a3c98a7
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-20 02:29:32 -0700

    Created an API to delete a user as an admin

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,8 +3,20 @@ import expressAsyncHandler from "express-async-handler";
 import bcrypt from "bcryptjs";
 import data from "../data.js";
 import User from "../models/userModel.js";
-import { generateToken, isAuth } from "../utils.js";
+import { generateToken, isAdmin, isAuth } from "../utils.js";
 const userRouter = express.Router();
+
+
+
+userRouter.get(
+  '/',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.send(users);
+  })
+);
 
 userRouter.get(
   "/seed",
@@ -90,4 +102,27 @@ userRouter.put(
     }
   })
 );
+
+userRouter.delete(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      if (user.email === 'admin@example.com') {
+        res.status(400).send({ message: 'Can Not Delete Admin User' });
+        return;
+      }
+      const deleteUser = await user.remove();
+      res.send({ message: 'User Deleted', user: deleteUser });
+    } else {
+      res.status(404).send({ message: 'User Not Found' });
+    }
+  })
+);
+
+
+
+
 export default userRouter;
