aaff7b7b751608eaaa9b0fb923f96176268d6ba4
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-21 06:19:56 -0700

    Added a seller object to save the name, logo, description,  rating and numReview

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
@@ -7,10 +7,17 @@ const userSchema = new mongoose.Schema(
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false, required: true },
     isSeller: { type: Boolean, default: false, required: true },
+    seller: {
+      name: String,
+      logo: String,
+      description: String,
+      rating: { type: Number, default: 0, required: true },
+      numReviews: { type: Number, default: 0, required: true },
+    },
   },
   {
     timestamps: true,
   }
 );
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 export default User;
